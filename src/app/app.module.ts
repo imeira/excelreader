31cb61caa2649232ff8d84f866da35cad0887b23
Igor Meira <meira.igor@gmail.com>
2021-02-08 21:01:45 -0300

    ng-csv-parse

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,16 +5,19 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ExcelsheetComponent } from './excelsheet/excelsheet.component';
 import { LoteComponent } from './lote/lote.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [	
     AppComponent,
     ExcelsheetComponent,
-      LoteComponent
+    LoteComponent
    ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule, 
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
