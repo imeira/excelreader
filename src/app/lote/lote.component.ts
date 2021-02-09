55deeb673a16f8bc2f38166c910735771c96169b
Igor Meira <meira.igor@gmail.com>
2021-02-09 18:56:35 -0300

    Update lote.component.ts

--- src/app/lote/lote.component.ts
+++ src/app/lote/lote.component.ts
@@ -1,8 +1,59 @@
-import { Component, OnInit, ViewChild } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { NgxCsvParser, NgxCSVParserError } from 'ngx-csv-parser';
 import * as XLSX from 'xlsx';
 
+const SCHEMA = {
+  "NOME_POUPADOR": "text",
+  "CPF_POUPADOR": "text",
+  "DT_NASC_POUPADOR": "text",
+  "ENDERECO": "text",
+  "NUMERO": "text",
+  "COMPLEMENTO": "text",
+  "MUNICIPIO": "text",
+  "BAIRRO": "text",
+  "UF": "text",
+  "CEP": "text",
+  "NR_PROCESSO": "text",
+  "CDPASTA": "text",
+  "UF_PROCESSO": "text"/*,
+  "ORGAO": "text",
+  "VARA": "text",
+  "COMARCA": "text",
+  "DT_AJUIZAMENTO": "text",
+  "TIPO_ACAO": "text",
+  "ADV	NOME_ADVOGADO": "text",
+  "CPF_ADVOGADO": "text",
+  "OAB	OAB_UF	CNPJ": "text",
+  "RAZAO_SOCIAL": "text",
+  "OPTANTE_SIMPLES": "text",
+  "VL_BRUTO_ACORDO": "text",
+  "VL_ACORDO_PAGO": "text",
+  "VL_HONORARIO": "text",
+  "VL_FEBRAPO": "text",
+  "VL_CUSTAS": "text",
+  "FORMA_ACORDO": "text",
+  "FORMA_HONORARIO": "text",
+  "QTD_PARC_ACORDO": "text",
+  "VL_PARC_ACORDO": "text",
+  "DEST_ACORDO": "text",
+  "DEST_HONORARIO": "text",
+  "NR_BANCO_POUPADOR": "text",
+  "DS_BANCO_POUPADOR": "text",
+  "NR_AGENCIA_POUPADOR": "text",
+  "NR_CONTA_POUPADOR": "text",
+  "DV_CONTA_POUPADOR": "text",
+  "DS_BANCO_ADVOGADO": "text",
+  "NR_BANCO_ADVOGADO": "text",
+  "NR_AGENCIA_ADVOGADO": "text",
+  "NR_CONTA_ADVOGADO": "text",
+  "DV_CONTA_ADVOGADO": "text",
+  "TIPO_PROCESSAMENTO": "text",
+  "ID_PORTAL_ORIGEM": "text",
+  "ID_PARCELA_ACORDO_ORIGEM": "text",
+  "ID_PARCELA_HONO_ORIGEM": "text",
+  "ID_PARCELA_FEBR_ORIGEM": "text"*/
+}
 
 @Component({
   selector: 'app-lote',
@@ -11,124 +62,191 @@ import * as XLSX from 'xlsx';
 })
 export class LoteComponent implements OnInit {
 
+  formulario: FormGroup;
+  titulo: string;
+
+  displayedColumns: string[] = [
+    'NOME_POUPADOR',
+    'CPF_POUPADOR',
+    'DT_NASC_POUPADOR',
+    'ENDERECO',
+    'NUMERO',
+    'COMPLEMENTO',
+    'MUNICIPIO',
+    'BAIRRO',
+    'UF',
+    /*'CEP',
+    'NR_PROCESSO',
+    'CDPASTA',
+    'UF_PROCESSO',
+    'ORGAO',
+    'VARA',
+    'COMARCA',
+    'DT_AJUIZAMENTO',
+    'TIPO_ACAO',
+    'ADV',
+    'CPF_ADVOGADO',
+    'OAB',
+    'RAZAO_SOCIAL',
+    'OPTANTE_SIMPLES',
+    'VL_BRUTO_ACORDO',
+    'VL_ACORDO_PAGO',
+    'VL_HONORARIO',
+    'VL_FEBRAPO',
+    'VL_CUSTAS',
+    'FORMA_ACORDO',
+    'FORMA_HONORARIO',
+    'QTD_PARC_ACORDO',
+    'VL_PARC_ACORDO',
+    'DEST_ACORDO',
+    'DEST_HONORARIO',
+    'NR_BANCO_POUPADOR',
+    'DS_BANCO_POUPADOR',
+    'NR_AGENCIA_POUPADOR',
+    'NR_CONTA_POUPADOR',
+    'DV_CONTA_POUPADOR',
+    'DS_BANCO_ADVOGADO',
+    'NR_BANCO_ADVOGADO',
+    'NR_AGENCIA_ADVOGADO',
+    'NR_CONTA_ADVOGADO',
+    'DV_CONTA_ADVOGADO',
+    'TIPO_PROCESSAMENTO',
+    'ID_PORTAL_ORIGEM',
+    'ID_PARCELA_ACORDO_ORIGEM',
+    'ID_PARCELA_HONO_ORIGEM',
+    'ID_PARCELA_FEBR_ORIGEM', */
+    '$$edit'
+  ];
+
+  dataSchema = SCHEMA;
+  
+  //dataSource = [];
+  dataSource: Array<any> = [];
+  
+  // csvRecords: [][];
+  csvRecords: Array<any> = [];
+
+  tableRows = [];
+
+  constructor(
+    private formBuilder: FormBuilder,
+    private ngxCsvParser: NgxCsvParser
+   ) { }
+   
+   ngOnInit() {
+     this.formulario = this.formBuilder.group({
+       busca: [null, Validators.required],
+     });
+   }
+
   // --> begin Editable table 
   editField: string;
-  awaitingCsvRecords: Array<any> = [];
+  // csvRecordsAwaiting: Array<any> = [];
 
   updateList(id: number, property: string, event: any) {
     const editField = event.target.textContent;
     this.csvRecords[id][property] = editField;
+    console.log('XXX 2 XXX updateList id', id);
+    console.log('XXX 2 XXX updateList property', property);
+    console.log('XXX 2 XXX updateList editField', editField);
+    console.log('XXX 2 XXX updateList new this.csvRecords', this.csvRecords);
   }
 
   remove(id: any) {
-    this.awaitingCsvRecords.push(this.csvRecords[id]);
     this.csvRecords.splice(id, 1);
+    console.log('XXX 2 XXX remove id', id);
+    console.log('XXX 2 XXX remove csvRecords', this.csvRecords);
   }
 
   add() {
-    if (this.awaitingCsvRecords.length > 0) {
-      const person = this.awaitingCsvRecords[0];
-      this.csvRecords.push(person);
-      this.awaitingCsvRecords.splice(0, 1);
-    }
+      const data: Array<any> = [];
+      this.csvRecords.push(data);
   }
 
   changeValue(id: number, property: string, event: any) {
     this.editField = event.target.textContent;
+    console.log('XXX 2 XXX changeValue id', id);
+    console.log('XXX 2 XXX changeValue property', property);
+    console.log('XXX 2 XXX changeValue editField', this.editField);
   }
 // --> end Editable table 
 
 
-
 // --> begin xlsx
-  formulario: FormGroup;
+onFileChange(evt: any) {
+  const target : DataTransfer =  <DataTransfer>(evt.target);
+ 
+  if (target.files.length !== 1) throw new Error('Cannot use multiple files');
 
-  data: [][];
-  titulo: string;
+  const reader: FileReader = new FileReader();
 
-  tableHeaders = [];
-  tableRows = [];
+  reader.onload = (e: any) => {
+    const bstr: string = e.target.result;
 
-  constructor(
-   private formBuilder: FormBuilder,
-    private ngxCsvParser: NgxCsvParser
-  ) { }
+    const wb: XLSX.WorkBook = XLSX.read(bstr, { type: 'binary' });
 
-  ngOnInit() {
-    this.formulario = this.formBuilder.group({
-      busca: [null, Validators.required],
-    });
-  }
+    const wsname : string = wb.SheetNames[0];
 
-  onFileChange(evt: any) {
-    this.csvRecords =  [];
+    this.titulo = evt.target.files[0].name;
 
-    const target : DataTransfer =  <DataTransfer>(evt.target);
+    const ws: XLSX.WorkSheet = wb.Sheets[wsname];
+    console.log('XXX 1 XXX ws', ws);
 
-    if (target.files.length !== 1) throw new Error('Cannot use multiple files');
 
-    const reader: FileReader = new FileReader();
 
-    reader.onload = (e: any) => {
-      const bstr: string = e.target.result;
 
-      const wb: XLSX.WorkBook = XLSX.read(bstr, { type: 'binary' });
 
-      const wsname : string = wb.SheetNames[0];
 
-      this.titulo = "1- " + evt.target.files[0].name;
-      //console.log(this.titulo);
 
-      const ws: XLSX.WorkSheet = wb.Sheets[wsname];
-
-      console.log('XXX 1 XXX ws', ws);
-
-      this.data = (XLSX.utils.sheet_to_json(ws, { header: 1 }));
-
-      console.log('XXX 1 XXX data', this.data);
-
-      this.tableHeaders = this.data[0];
-      
     
 
-      let rows = this.data.slice(1);
-      console.log('XXX 1 XXX data.slice(1)', rows);
+    this.csvRecords = (XLSX.utils.sheet_to_json(ws, { header: 1 }));
+    console.log('XXX 1 XXX csvRecords', this.csvRecords);
 
-      this.tableRows = [rows];
+    this.dataSource = this.csvRecords;
+    console.log('XXX 1 XXX dataSource', this.dataSource);
 
-    };
+    /* Validação colunas*/
+    console.log('XXX 1 XXX header_diff', this.header_diff(this.displayedColumns,this.csvRecords[0]))
 
-    reader.readAsBinaryString(target.files[0]);
+    this.displayedColumns = this.csvRecords[0];
+    this.displayedColumns.push('$$edit');
+    
+    console.log('XXX 1 XXX displayedColumns', this.displayedColumns);
+    
+    let rows = this.csvRecords.slice(1);
+    console.log('XXX 1 XXX csvRecords.slice(1)', rows);
 
-  }
+  };
+
+  reader.readAsBinaryString(target.files[0]);
+
+  // Select the files from the event
+  const files = evt.srcElement.files;
+  console.log('XXX 2 XXX files', files);
+  
+  // Parse the file you want to select for the operation along with the configuration
+  this.ngxCsvParser.parse(files[0], { header: true, delimiter: ';' })
+  .pipe().subscribe((result: any) => {
+    console.log('XXX 2 XXX Result', result);
+    // this.csvRecords = result;
+    this.dataSource = result;
+  }, (error: NgxCSVParserError) => {
+    console.log('XXX 2 XXX Error', error);
+  });
+
+  
+}
+
+header_diff (a1 : any[], a2 : any[]) {
+
+  let intersectionA = a1.filter(x=> a2.includes(x))
+  let diferenceA = a1.filter(x => !intersectionA.includes(x))
+
+  return diferenceA;
+
+
+}
 // --> end xlsx
-
-// --> begin ngx-csv-parser
-  csvRecords: Array<any> = [];
-  header: boolean = true;
-
-
-  @ViewChild('fileImportInput', { static: false }) fileImportInput: any;
-
-   // Your applications input change listener for the CSV File
-   fileChangeListener($event: any): void {
-    this.data =  [];
- 
-    // Select the files from the event
-    const files = $event.srcElement.files;
-
-    this.titulo = "2- " + files[0].name;
- 
-    // Parse the file you want to select for the operation along with the configuration
-    this.ngxCsvParser.parse(files[0], { header: this.header, delimiter: ';' })
-    .pipe().subscribe((result: Array<any>) => {
-      console.log('XXX 2 XXX Result', result);
-      this.csvRecords = result;
-    }, (error: NgxCSVParserError) => {
-      console.log('XXX 2 XXX Error', error);
-    });
-  }
-// --> end ngx-csv-parser
-
 
 }
