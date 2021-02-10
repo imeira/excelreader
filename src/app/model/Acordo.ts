8c541bba6e5db604a48c0ceb7ad9f0f83fe4cd28
Igor Meira <meira.igor@gmail.com>
2021-02-10 19:26:24 -0300

    Update Acordo.ts

---
+++ src/app/model/Acordo.ts
@@ -0,0 +1,12 @@
+
+export interface Acordo {
+    NOME_POUPADOR: string;
+    CPF_POUPADOR: string;
+    DT_NASC_POUPADOR: string;
+    ENDERECO: string;
+    NUMERO: string;
+    COMPLEMENTO: string;
+    MUNICIPIO: string;
+    BAIRRO: string;
+    UF:string;
+  }
