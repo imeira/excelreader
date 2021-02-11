64dc06e92881afac287eb9a45c5239691046c589
Igor Meira <meira.igor@gmail.com>
2021-02-11 00:54:30 -0300

    aceitando xls

--- src/app/lote/lote.component.ts
+++ src/app/lote/lote.component.ts
@@ -4,9 +4,7 @@ import { MatSort } from '@angular/material/sort';
 import { MatTableDataSource } from '@angular/material/table';
 import { NgxCsvParser, NgxCSVParserError } from 'ngx-csv-parser';
 import * as XLSX from 'xlsx';
-
 import {  WorkBook, read, utils, write, readFile } from 'xlsx';
-
 import { Acordo } from '../model/Acordo';
 
 
@@ -35,13 +33,8 @@ const COLUMNS = [
   '$$edit'
 ];
 
-
-
 const ELEMENT_DATA: Acordo[] = [];
-
-
 type AOA = any[][];
-
 
 @Component({
   selector: 'app-lote',
@@ -58,21 +51,16 @@ export class LoteComponent implements OnInit {
   formulario: FormGroup;
   titulo: string;
 
+  data!: AOA;
   displayedColumns: string[] = COLUMNS;
   dataSchema = SCHEMA;
   dataSource = new MatTableDataSource(ELEMENT_DATA);
 
   @ViewChild(MatSort) sort: MatSort;
 
-
-  data!: AOA;
-
-  willDownload = false;
-
-  // --> begin Editable table
+  willDownload = true;
   editField: string;
 
-// --> end Editable table
 
   file!: File;
 
@@ -86,11 +74,11 @@ export class LoteComponent implements OnInit {
      });
    }
 
-   applyFilter(filterValue: string) {
-    filterValue = filterValue.trim(); // Remove whitespace
-    filterValue = filterValue.toLowerCase(); // MatTableDataSource defaults to lowercase matches
-    this.dataSource.filter = filterValue;
-  }
+  //  applyFilter(filterValue: string) {
+  //   filterValue = filterValue.trim(); // Remove whitespace
+  //   filterValue = filterValue.toLowerCase(); // MatTableDataSource defaults to lowercase matches
+  //   this.dataSource.filter = filterValue;
+  // }
 
   confirmEdit(event: any){
     console.log('XXXXXX confirmEdit event', event);
@@ -126,8 +114,6 @@ export class LoteComponent implements OnInit {
     console.log('XXXXXX updateList new this.dataSource', this.dataSource);
   }
 
-
-
   remove(id: any) {
     console.log('XXXXXX remove id', id);
     this.dataSource.data.splice(id, 1);
@@ -143,16 +129,12 @@ export class LoteComponent implements OnInit {
       this.dataSource._updateChangeSubscription();
   }
 
-
-
   changeValue(id: number, property: string, event: any) {
     console.log('XXXXXX changeValue id', id);
     console.log('XXXXXX changeValue property', property);
     this.editField = event.target.textContent;
     console.log('XXXXXX changeValue editField', this.editField);
   }
-
-// --> begin xlsx
 
   onFileChange(evt: any) {
     const target: DataTransfer =  (evt.target) as DataTransfer;
@@ -162,108 +144,47 @@ export class LoteComponent implements OnInit {
     const reader: FileReader = new FileReader();
 
     this.file = target.files[0];
-
+    this.titulo = this.file.name;
     let workbookkk;
     let xLRowObject;
-    let parsedJSON = [];
+    // let parsedJSON = [];
     let jsonObject;
     reader.onload = (e: any) => {
       //  alert(reader.result);
       const data = reader.result;
       workbookkk = read(data, {type: 'binary'});
       console.log('XXXXXX convertExcelToJson workbookkk', workbookkk);
-      // Here is your object
+
+      const ws: XLSX.WorkSheet = workbookkk.Sheets[workbookkk.SheetNames[0]];
+      console.log('XXXXXX ws', ws);
+
+      /* save data */
+      this.data = ((XLSX.utils.sheet_to_json(ws, {header: 1})) as AOA);
+      console.log('XXXXXX data', this.data);
+
+      /* Validação colunas*/
+      console.log('XXXXXX header_diff', this.header_diff(this.displayedColumns, this.dataSource.data));
+
+      this.displayedColumns = this.data[0];
+      this.displayedColumns.push('$$edit');
+      console.log('XXXXXX displayedColumns', this.displayedColumns);
+
       xLRowObject = utils.sheet_to_json(workbookkk.Sheets[workbookkk.SheetNames[0]]);
       jsonObject = JSON.stringify(xLRowObject);
       console.log(jsonObject);
       console.log('XXXXXX convertExcelToJson jsonObject', jsonObject);
       console.log(xLRowObject);
       console.log('XXXXXX convertExcelToJson xLRowObject', xLRowObject);
-      parsedJSON = JSON.parse(jsonObject);
-      console.log('XXXXXX convertExcelToJson parsedJSON', parsedJSON);
+      // parsedJSON = JSON.parse(jsonObject);
+      // console.log('XXXXXX convertExcelToJson parsedJSON', parsedJSON);
       this.dataSource.data = xLRowObject;
       console.log('XXXXXX this.dataSource.data ', this.dataSource.data);
+      this.setDownload(jsonObject);
     };
+
     reader.readAsBinaryString(target.files[0]);
 
-      // document.getElementById('output').innerHTML = dataString.slice(0, 300).concat('...');
-      // this.setDownload(dataString);
-  }
-
-  onFileChangessss(ev) {
-    // let workBook = null;
-    // let jsonData = null;
-    // const reader = new FileReader();
-    const file = ev.target.files[0];
-    this.convertExcelToJson(file);
-    console.log('XXXXXX 111 onFileChange this.dataSource', this.dataSource);
-    //
-    // reader.onload = (event) => {
-    //   const data = reader.result;
-    //   workBook = XLSX.read(data, { type: 'binary' });
-    //   jsonData = workBook.SheetNames.reduce((initial, name) => {
-    //     const sheet = workBook.Sheets[name];
-    //     initial[name] = XLSX.utils.sheet_to_json(sheet);
-    //     return initial;
-    //   }, {});
-    //   console.log('XXXXXX onFileChange jsonData', jsonData);
-    //   const dataString = JSON.stringify(jsonData);
-    //   console.log('XXXXXX onFileChange dataString', dataString);
-    //
-    //   const parsedJSON = JSON.parse(dataString);
-    //   // tslint:disable-next-line:prefer-for-of
-    //   console.log('XXXXXX onFileChange parsedJSON', parsedJSON);
-    //   // console.log('XXXXXX onFileChange parsedJSON.length', parsedJSON);
-    //
-    //   // for (const val of parsedJSON) {
-    //   //   console.log('XXXXXX onFileChange val', val);
-    //   // };
-    //
-    //   ELEMENT_DATA.push(parsedJSON);
-    //   console.log('XXXXXX onFileChange ELEMENT_DATA', ELEMENT_DATA);
-    //
-    //   // this.dataSource = new MatTableDataSource(ELEMENT_DATA);
-    //
-    //   console.log('XXXXXX onFileChange this.dataSource.data', this.dataSource.data);
-    //
-    //   // document.getElementById('output').innerHTML = dataString.slice(0, 300).concat('...');
-    //   // this.setDownload(dataString);
-    // };
-    // reader.readAsBinaryString(file);
-  }
-
-  convertExcelToJson(file)
-  {
-    const reader = new FileReader();
-    let workbookkk;
-    let xLRowObject;
-    let parsedJSON = [];
-    let jsonObject;
-    reader.readAsBinaryString(file);
-    return new Promise((resolve, reject) => {
-      // tslint:disable-next-line:only-arrow-functions
-      reader.onload = function(){
-        //  alert(reader.result);
-        const data = reader.result;
-        workbookkk = read(data, {type: 'binary'});
-        console.log('XXXXXX convertExcelToJson workbookkk', workbookkk);
-        workbookkk.SheetNames.forEach(function(sheetName) {
-          // Here is your object
-          xLRowObject = utils.sheet_to_json(workbookkk.Sheets[sheetName]);
-          jsonObject = JSON.stringify(xLRowObject);
-          console.log(jsonObject);
-          console.log('XXXXXX convertExcelToJson jsonObject', jsonObject);
-          console.log(xLRowObject);
-          console.log('XXXXXX convertExcelToJson xLRowObject', xLRowObject);
-          parsedJSON = JSON.parse(jsonObject);
-          console.log('XXXXXX convertExcelToJson parsedJSON', parsedJSON);
-          resolve(xLRowObject);
-        });
-      };
-    });
-    // ELEMENT_DATA.push(xLRowObject);
-    // this.dataSource = new MatTableDataSource(ELEMENT_DATA);
-    this.dataSource.data = parsedJSON;
+  // document.getElementById('output').innerHTML = dataString.slice(0, 300).concat('...');
   }
 
   setDownload(data) {
@@ -271,91 +192,16 @@ export class LoteComponent implements OnInit {
     setTimeout(() => {
       const el = document.querySelector('#download');
       el.setAttribute('href', `data:text/json;charset=utf-8,${encodeURIComponent(data)}`);
-      el.setAttribute('download', 'xlsxtojson.json');
+      el.setAttribute('download', 'modelo.csv');
     }, 1000);
   }
 
 
-
-
-  onFileChangeOLD(evt: any) {
-  const target: DataTransfer =  (evt.target) as DataTransfer;
-
-  if (target.files.length !== 1) { throw new Error('Cannot use multiple files'); }
-
-  const reader: FileReader = new FileReader();
-
-  this.file = target.files[0];
-
-  // reader.readAsText(this.file);
-  // reader.onload = (e) => {
-  //   let csv: string = reader.result as string;
-  //   console.log('XXXXXX csv', csv);
-  // }
-
-  reader.onload = (e: any) => {
-    const bstr: string = e.target.result;
-
-    const wb: XLSX.WorkBook = XLSX.read(bstr, { type: 'binary' });
-
-    const wsname: string = wb.SheetNames[0];
-
-    this.titulo = evt.target.files[0].name;
-
-    const ws: XLSX.WorkSheet = wb.Sheets[wsname];
-    console.log('XXXXXX ws', ws);
-
-     /* save data */
-    this.data = ((XLSX.utils.sheet_to_json(ws, {header: 1})) as AOA);
-    console.log('XXXXXX data', this.data);
-    console.log('XXXXXX dataSource', this.dataSource);
-
-
-    /* Validação colunas*/
-    console.log('XXXXXX header_diff', this.header_diff(this.displayedColumns, this.dataSource.data));
-
-    this.displayedColumns = this.data[0];
-    this.displayedColumns.push('$$edit');
-
-    console.log('XXXXXX displayedColumns', this.displayedColumns);
-
-    const rows = this.dataSource.data.slice(1);
-    console.log('XXXXXX dataSource.slice(1)', rows);
-
-  };
-
-  reader.readAsBinaryString(target.files[0]);
-
-  // Select the files from the event
-  const files = evt.srcElement.files;
-  console.log('XXXXXX files', files);
-
-  // Parse the file you want to select for the operation along with the configuration
-  this.ngxCsvParser.parse(files[0], { header: true, delimiter: ';' })
-  .pipe().subscribe((result: any) => {
-    console.log('XXXXXX Result', result);
-    this.dataSource.data = result;
-    console.log('XXXXXX this.dataSource.data ', this.dataSource.data );
-  }, (error: NgxCSVParserError) => {
-    console.log('XXXXXX Error', error);
-  });
-
-
-}
-
-header_diff(a1: any[], a2: Acordo[]) {
-
-  const intersectionA = a1.filter(x => a2.includes(x));
-  const diferenceA = a1.filter(x => !intersectionA.includes(x));
-  console.log('XXXXXX header_diff', diferenceA);
-
-  return diferenceA;
-
-
-}
-
-
-
-// --> end xlsx
+  header_diff(a1: any[], a2: Acordo[]) {
+    const intersectionA = a1.filter(x => a2.includes(x));
+    const diferenceA = a1.filter(x => !intersectionA.includes(x));
+    console.log('XXXXXX header_diff', diferenceA);
+    return diferenceA;
+  }
 
 }
