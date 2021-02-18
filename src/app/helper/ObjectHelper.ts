1ace09f85bb86783998b1edbdb8c5f59b43156da
Igor Meira <meira.igor@gmail.com>
2021-02-18 11:14:46 -0300

    Update ObjectHelper.ts

--- src/app/helper/ObjectHelper.ts
+++ src/app/helper/ObjectHelper.ts
@@ -4,7 +4,7 @@ export class ObjectHelper {
   static printNamesAndValues<T>(obj: T) {
     const propNames = Object.getOwnPropertyNames(obj);
     propNames.forEach(
-      function(propName) {
+      propName => {
         console.log(
           'name: ' + propName
           + ' value: ' + obj[propName]);
@@ -13,51 +13,51 @@ export class ObjectHelper {
   }
 
   static printTypeNames<T>(obj: T) {
-    const objectKeys = Object.keys(obj) as Array<keyof T>;
+    const objectKeys = Object.keys(obj) as (keyof T)[];
     for (const key of objectKeys) {
       console.log('key:' + key);
     }
   }
 
-  static  describeClass( typeOfClass: any) {
+  static describeClass(typeOfClass: any): any {
     const a = new typeOfClass();
-    const array = Object.getOwnPropertyNames(a);
-    return array;
+    return Object.getOwnPropertyNames(a);
   }
 
-  static describe(instance): Array<string> {
+  static describe(instance: any): any {
     return Object.getOwnPropertyNames(instance);
   }
 
-  // tslint:disable-next-line:ban-types
-  static enumToArray(e: any): Object[] {
+  static enumToArray(e: any): any[] {
     return Object.keys(e)
-              .map(key => ({ id: e[key], name: key }))
+      .map(key => ({ id: e[key], name: key }));
   }
 
   static getNamesAndValues<T extends number>(e: any) {
-      return ObjectHelper.getNames(e).map(n => ({ name: n, value: e[n] as T }));
+    return ObjectHelper.getNames(e).map(n => ({ name: n, value: e[n] as T }));
   }
 
   static getNames(e: any) {
-      return ObjectHelper.getObjValues(e).filter(v => typeof v === 'string') as string[];
+    return ObjectHelper.getObjValues(e).filter(v => typeof v === 'string') as string[];
   }
 
   static getValues<T extends number>(e: any) {
-      return ObjectHelper.getObjValues(e).filter(v => typeof v === 'number') as T[];
+    return ObjectHelper.getObjValues(e).filter(v => typeof v === 'number') as T[];
   }
 
   static getSelectList<T extends number, U>(e: any, stringConverter: (arg: U) => string) {
-      const selectList = new Map<T, string>();
-      this.getValues(e).forEach(val => selectList.set(val as T, stringConverter(val as unknown as U)));
-      return selectList;
+    const selectList = new Map<T, string>();
+    // tslint:disable-next-line:no-static-this
+    this.getValues(e).forEach(val => selectList.set(val as T, stringConverter(val as unknown as U)));
+    return selectList;
   }
 
   static getSelectListAsArray<T extends number, U>(e: any, stringConverter: (arg: U) => string) {
-      return Array.from(this.getSelectList(e, stringConverter), value => ({ value: value[0] as T, presentation: value[1] }));
+    // tslint:disable-next-line:no-static-this
+    return Array.from(this.getSelectList(e, stringConverter), value => ({ value: value[0] as T, presentation: value[1] }));
   }
 
   private static getObjValues(e: any): (number | string)[] {
-      return Object.keys(e).map(k => e[k]);
+    return Object.keys(e).map(k => e[k]);
   }
 }
