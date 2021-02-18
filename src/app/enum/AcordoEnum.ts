dcd098b1c457dacbbd596dc8aa222484f608692d
Igor Meira <meira.igor@gmail.com>
2021-02-18 11:09:14 -0300

    Update AcordoEnum.ts

--- src/app/enum/AcordoEnum.ts
+++ src/app/enum/AcordoEnum.ts
@@ -1,16 +1,11 @@
 export enum AcordoEnum {
-  NOME_POUPADOR  = 'PoupadorNome',
+  NOME_POUPADOR = 'PoupadorNome',
   CPF_POUPADOR = 'PoupadorCPF',
-  DT_NASC_POUPADOR    = 'PoupadorDtNascimento',
-  ENDERECO    = 'PoupadorEndereco',
-  NUMERO    = 'PoupadorEnderecoNumero',
-  COMPLEMENTO    = 'PoupadorEnderecoComplemento',
-  MUNICIPIO    = 'PoupadorEnderecoMunicipio',
-  BAIRRO    = 'PoupadorEnderecoBairro',
-  UF    = 'PoupadorEnderecoUF'
+  DT_NASC_POUPADOR = 'PoupadorDtNascimento',
+  ENDERECO = 'PoupadorEndereco',
+  NUMERO = 'PoupadorEnderecoNumero',
+  COMPLEMENTO = 'PoupadorEnderecoComplemento',
+  MUNICIPIO = 'PoupadorEnderecoMunicipio',
+  BAIRRO = 'PoupadorEnderecoBairro',
+  UF = 'PoupadorEnderecoUF'
 }
-
-const keys = Object.keys(AcordoEnum);
-const values =  Object.values(AcordoEnum);
-const entries =  Object.entries(AcordoEnum);
-const assign =  Object.assign(AcordoEnum);
