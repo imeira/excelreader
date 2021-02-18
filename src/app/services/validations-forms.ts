3b442055cc3fd386a43f5b85dc87ba58142def9d
Igor Meira <meira.igor@gmail.com>
2021-02-18 11:29:25 -0300

    Update validations-forms.ts

--- src/app/services/validations-forms.ts
+++ src/app/services/validations-forms.ts
@@ -1,38 +1,35 @@
 import { FormArray, FormControl, FormGroup } from '@angular/forms';
+
 export class FormValidations {
 
-  static requireMinCheckbox(min = 1 ){
-    const validator = ( fromarray: FormArray) => {
+  static requireMinCheckbox(min = 1) {
+    return (fromarray: FormArray) => {
       const totalcheckd = fromarray.controls.map(v => v.value).reduce((tot, curr) => curr ? tot + 1 : tot, 0);
-      return totalcheckd >=  min  ? null : {required : true};
+      return totalcheckd >= min ? null : { required : true };
     };
-    return validator;
   }
 
-  static cepvalidator(control: FormControl){
+  static cepvalidator(control: FormControl) {
     const cep = control.value;
     const validacao = /[0-9]{5}-[0-9]{3}/;
-    if (cep && cep !== ''){
-      return validacao.test(cep) ? null : {cepinvalid : true};
+    if (cep && cep !== '') {
+      return validacao.test(cep) ? null : { cepinvalid : true };
     }
     return null;
   }
 
   static equalsTo(other: string) {
-    const comparewith = (control: FormGroup) => {
+    return (control: FormGroup) => {
       const campo1 = control;
       const campo2 = control.root.get(other) as FormControl;
 
-      if (campo2 !== null && campo1 !== null){
-        if (campo1.value !== campo2.value){
-          return {isnotequal: true};
-        } else {
-          return null;
-        }
-      }
+      if (campo2 !== null && campo1 !== null) {
+        if (campo1.value !== campo2.value) {
+          return { isnotequal: true };
+        };
+      };
       return null;
     };
-    return comparewith;
   }
 
 
